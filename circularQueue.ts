0e80ffb76465356418364185c18a6969b3a4a55c
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-08-26 22:24:52 +0530

    final  circular queue

--- circularQueue.ts
+++ circularQueue.ts
@@ -1,23 +1,50 @@
 class CircularQueue {
   private queue;
-  private rear = 0;
-  private front = -1;
-  private size = 5;
-  constructor(array) {
-    this.queue = array;
+  private rear;
+  private front;
+  private size;
+  private capacity;
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.queue = new Array(capacity).fill(null);
+    this.rear = -1;
+    this.front = -1;
   }
 
   enque(element: number) {
-    this.rear = (this.rear + 1) % this.size;
+    if (this.isFull()) {
+      console.log("queue overflows");
+      return;
+    }
+    this.rear = (this.rear + 1) % this.capacity;
     this.queue[this.rear] = element;
+    this.size++;
   }
   deque() {
-    this.front = (this.front + 1) % this.size;
+    if (this.isEmpty()) {
+      console.log("Queue underflows");
+      return;
+    }
+
+    this.front = (this.front + 1) % this.capacity;
     this.queue[this.front] = null;
+    if (this.front == this.rear) {
+      this.front = -1;
+      this.rear = -1;
+      this.size--;
+    }
   }
+
+  isFull() {
+    return this.capacity == this.size;
+  }
+  isEmpty() {
+    return this.front == -1;
+  }
+
   display() {
     for (let i = this.front; i < this.rear; i++) {
-      console.log(this.queue[i]);
+      if (this.queue[i]) console.log(this.queue[i]);
     }
   }
 }
